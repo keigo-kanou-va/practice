2ed8e2a80a814b3d39bcfa4e8e58daae7bb26423
keigo.kanou.va <keigo.kanou.va@nttdocomo.com>
2024-12-13 16:30:32 +0000

    ページ間の値渡し

--- lib/main.dart
+++ lib/main.dart
@@ -39,27 +39,25 @@ class MyHomePage extends StatefulWidget {
 
 //_MyHomePageStateを定義
 class _MyHomePageState extends State<MyHomePage> {
- 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Padding(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Padding(
           padding: const EdgeInsets.all(20.0),
           child: ElevatedButton(
             onPressed: () {
-               Navigator.of(context).push<void>( 
-               MaterialPageRoute(
-               builder: (context) => const DetailPage(),
-          ),
-        );
-            }, 
+              Navigator.of(context).push<void>(
+                MaterialPageRoute(
+                  builder: (context) => const DetailPage(),
+                ),
+              );
+            },
             child: Text('次へ'),
-            )
-        ),
+          )),
     );
   }
 }
@@ -70,66 +68,72 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text('詳細ページ'),
-        ),
-        body: 
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      showModalBottomSheet<void>(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return const Detail2Page();
-                          },
-                          isScrollControlled: true, // フルスクリーンで表示
-                        );
-                    }, 
-                    child: Text('また次へ'),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push<void>( 
-                      MaterialPageRoute(
-                      builder: (context) => const Detail2Page(),
-                      ),
-                      );// モーダルを閉じる
-                    }, 
-                    child: Text('戻る'),
-                    ),
-                ],
-              ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('詳細ページ'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Detail2Page(id: 1, name: 'モーダル',);
+                  },
+                  isScrollControlled: true, // フルスクリーンで表示
+                );
+              },
+              child: Text('また次へ'),
             ),
-        );
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push<void>(
+                  MaterialPageRoute(
+                    builder: (context) => Detail2Page(id: 2, name: 'プッシュ',),
+                  ),
+                ); // モーダルを閉じる
+              },
+              child: Text('戻る'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
 class Detail2Page extends StatelessWidget {
-  const Detail2Page({super.key});
+  Detail2Page({
+    super.key,
+    required this.id,
+    required this.name,
+  });
+
+  final int id;
+  final String name;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('最終ページ'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: ElevatedButton(
-            onPressed: () {
+          onPressed: () {
             Navigator.of(context).pop(); // モーダルを閉じる
-            }, 
-          child: const Text('終点'),
-            ),
-        ), 
-        );
+          },
+          child: Text('id = ${id}、name = ${name}'),
+        ),
+      ),
+    );
   }
 }
